32b53874dc38de294ac4d89c8e3f00b04497fe01
Choi Wonnho <wonnho@nate.com>
2026-05-13 23:29:23 +0900

    brew install tomcat9,run/edit configuration-> deplaoyment, folder tree, web.xml, dispatcher-servlet.xml, set up .war

--- build.gradle.kts
+++ build.gradle.kts
@@ -9,18 +9,23 @@ version = "1.0-SNAPSHOT"
 repositories {
     mavenCentral()
 }
+
 dependencies {
-    // Spring 5 Framework BOM
     implementation(platform("org.springframework:spring-framework-bom:5.3.31"))
 
-    // Core Spring dependencies
     implementation("org.springframework:spring-context")
     implementation("org.springframework:spring-webmvc")
 
-    // Your existing JUnit 5 dependencies
+    // Tomcat 9 provides this at runtime
+    compileOnly("javax.servlet:javax.servlet-api:4.0.1")
+
+    // Optional if using JSP/JSTL
+    implementation("javax.servlet:jstl:1.2")
+
     testImplementation(platform("org.junit:junit-bom:5.10.0"))
     testImplementation("org.junit.jupiter:junit-jupiter")
 }
+
 tasks.test {
     useJUnitPlatform()
 }
